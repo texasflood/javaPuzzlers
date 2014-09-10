6e5209175487d7af7c7ecf7d30bcb29e8687b6e0
Anas Syed <anas27@gmail.com>
2014-09-10 18:20:10 +0100

    First commit

---
+++ 2-expressive-puzzlers/puzzle-1/Oddity.java
@@ -0,0 +1,9 @@
+public class Oddity {
+    public static boolean isOdd(int i) {
+        return i % 2 == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(-5));
+    }
+}
